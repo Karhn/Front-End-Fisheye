7781c6ece11cffe435908a3de8ecb903200ca125
Gaston Dubois <drkarhn@gmail.com>
2025-07-31 17:59:10 +0200

    Accessibilité de la page index.html + navigation entre la page index et page photographe

--- scripts/pages/photographer.js
+++ scripts/pages/photographer.js
@@ -1,1 +1,20 @@
-//Mettre le code JavaScript lié à la page photographer.html
+function getPhotographerIdFromURL() {
+    const params = new URLSearchParams(window.location.search);
+    return parseInt(params.get("id"));
+}
+
+async function getPhotographerData() {
+    const response = await fetch("../data/photographers.json");
+    const data = await response.json();
+    const photographerId = getPhotographerIdFromURL()
+    const photographer = data.photographers.find((p) => p.id === photographerId);
+
+    console.log("Photogrape Séléctionné :", photographer);
+    return photographer;
+}
+
+async function init() {
+    const photographer = await getPhotographerData();
+}
+
+init();
--- scripts/templates/photographer.js
+++ scripts/templates/photographer.js
@@ -1,5 +1,5 @@
 function photographerTemplate(data) {
-    const { name, portrait, country, city, tagline, price } = data;
+    const { name, portrait, country, city, tagline, price, id } = data;
 
     const picture = `assets/photographers/${portrait}`;
 
@@ -7,11 +7,21 @@ function photographerTemplate(data) {
 
         const article = document.createElement( 'article' );
 
+        const link = document.createElement( 'a' );
+        link.setAttribute("href", `photographer.html?id=${id}`);
+        link.setAttribute("aria-label", name)
+        link.setAttribute("tabindex", "0");
+
         const img = document.createElement( 'img' );
-        img.setAttribute("src", picture)
+        img.setAttribute("src", picture);
+        img.setAttribute("alt", "");
 
         const h2 = document.createElement( 'h2' );
         h2.textContent = name;
+
+        link.appendChild(img);
+        link.appendChild(h2);
+        article.appendChild(link);
 
         const location = document.createElement( 'p' );
         location.classList.add("photographer-location");
@@ -25,8 +35,7 @@ function photographerTemplate(data) {
         priceElement.classList.add("photographer-price");
         priceElement.textContent = `${price}€/jour`;
 
-        article.appendChild(img);
-        article.appendChild(h2);
+
         article.appendChild(location);
         article.appendChild(taglineElement);
         article.appendChild(priceElement);
